85572142de73d4277d3cf22b6104e76bd29d46b9
masterthuku <peterthuku706@gmail.com>
2025-02-16 00:40:59 +0300

    more UI

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,8 +1,21 @@
+import UserCard from '@/components/UserCard'
 import React from 'react'
 
 const AdminPage = () => {
   return (
-    <div>AdminPage</div>
+    <div className='flex p-4 gap-4 flex-col md:flex-row'>
+      {/* left */}
+      <div className='w-full lg:w-2/3'>
+      <div className='flex gap-4 justify-between flex-wrap'>
+        <UserCard type='student'/>
+        <UserCard type='teacher'/>
+        <UserCard type='parent'/>
+        <UserCard type='staff'/>
+      </div>
+      </div>
+      {/* right */}
+      <div className='w-full lg:w-1/3'>r</div>
+    </div>
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,18 +4,24 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between p-4">
-      <div className="hidden md:flex">
+      <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
         <Image src="/search.png" alt="" width={14} height={14} />
-        <input placeholder="Search" type="text" />
+        <input placeholder="Search" type="text" className="w-[200px] p-2 bg-transparent outline-none" />
       </div>
 
-      <div className="">
+      <div className="flex items-center gap-6 justify-end w-full">
         <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
           <Image src="/message.png" alt="" width={20} height={20} />
         </div>
-        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer relative">
           <Image src="/announcement.png" alt="" width={20} height={20} />
+          <div className="flex absolute -top-3 -right-3 w-5 h-5 items-center justify-center bg-purple-500 text-white rounded-full text-xs">1</div>
         </div>
+        <div className="flex flex-col">
+          <span className="text-xs leading-3 font-medium">Peter Thuku</span>
+          <span className="text-[10px] text-gray-500 text-right">Admin</span>
+        </div>
+        <Image src="/avatar.png" alt="" width={40} height={40} className="rounded-full" />
       </div>
     </div>
   );
