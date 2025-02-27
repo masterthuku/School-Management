b83c2ea07cdd6181438797717d3cf36d2a714cdd
masterthuku <peterthuku706@gmail.com>
2025-02-27 23:39:45 +0300

    more backend

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -121,6 +121,9 @@ const TeacherListPage = async ({
             break;
           case "search":
             query.name = {contains: value, mode: "insensitive"};
+            break;
+          default:
+            break;
         }
       }
     }
